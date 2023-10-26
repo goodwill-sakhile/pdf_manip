8445274202603a72d028b0e36f16af614806c5ee
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2023-10-26 10:05:09 +0200

    file manager script

--- file_manager.py
+++ file_manager.py
@@ -1,10 +1,13 @@
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.screen import MDScreen
+from kivy.uix.screenmanager import SlideTransition
+from kivy.uix.filechooser import FileChooserIconView
+from touch import TouchBox
 from kivy.lang import Builder
-
 root = Builder.load_string("""
 <SelectFileScreen>:
+	id:select_file_screen
 	name:"select_file_screen"
 	MDBoxLayout:
 		md_bg_color:[220/float(255), 220/float(255), 220/float(255), 1]
@@ -20,43 +23,56 @@ root = Builder.load_string("""
 				orientation:"vertical"
 				Widget:
 				MDIconButton:
+					root:select_file_screen
 					size_hint:None, None
-					sie:"30dp", "30dp"
-					icon_size:"30dp"
+					size:"50dp", "50dp"
 					icon:"arrow-left"
-					pos_hint:{"center_x":.5, "centery":.05}
-				Widget:
-		MDBoxLayout:
-			orientation:"vertical"
-			MDBoxLayout:
-				size_hint_y:None
-				height:"50dp"
-				Widget:
-				MDIconButton:
-					icon:"undo"
-					size_hint:None, None
-					size:"40dp", "40dp"
+					pos_hint:{"center_x":.5, "center_y":.5}
 					icon_size:"30dp"
+					theme_text_color:"Custom"
+					text_color:[1, 1, 1, 1]
+					on_press:root.goBackToHomeScreen()
 				Widget:
-			FileChooserIconView:
+			MDBoxLayout:
+				MDLabel:
+					text:"File System"
+					font_size:"18dp"
+					text_size:self.size
+					halign:"center"
+					valign:"middle"
+					color:[1, 1, 1, 1]
+			MDBoxLayout:
+				size_hint_x:None
+				width:"50dp"
+		MDBoxLayout:
+			MyFiles:
+				id:files_layout
 		MDBoxLayout:
 			size_hint_y:None
 			height:"60dp"
 			padding:10
-			MDBoxLayout: 
-				md_bg_color:[0, 0, 0, 1]
+			NextButton: 
+				root:select_file_screen
+				md_bg_color:[0, 154/float(255), 220/float(255), 1]
 				radius:[40, 40, 40, 40]
 				MDLabel:
 					text:"Next"
-					bold:True
 					font_size:"18dp"
 					text_size:self.size
 					halign:"center"
 					valign:"middle"
 					color:[1, 1, 1, 1]
 """)
-class SelectFileScreen(MDScreen):
+class NextButton(TouchBox):
+	def respondToTouch(self):
+		if len(self.root.ids.files_layout.selection) >= 1:
+			pass
+class MyFiles(FileChooserIconView):
 	pass
+class SelectFileScreen(MDScreen):
+	def goBackToHomeScreen(self):
+		self.parent.transition = SlideTransition(direction = "right")
+		self.parent.current = "home_screen"
 class App(MDApp):
 	def build(self):
 		root = SelectFileScreen()
